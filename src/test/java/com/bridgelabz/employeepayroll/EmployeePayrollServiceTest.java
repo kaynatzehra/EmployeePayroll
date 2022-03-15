818344c1fa30dcf83e498b0f49afa41f63aa4121
Kaynat Zehra <kaynatzehra23@gmail.com>
2022-03-15 17:49:50 +0530

    Added: Employee Payroll Program

---
+++ src/test/java/com/bridgelabz/employeepayroll/EmployeePayrollServiceTest.java
@@ -0,0 +1,37 @@
+package com.bridgelabz.employeepayroll;
+import java.util.Arrays;
+
+import com.bridgelabz.employeepayroll.EmployeePayrollService.IOService;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class EmployeePayrollServiceTest
+{
+    @Test
+    public void given3Employees_WhenWrittenToFile_ShouldMatchEmployeeEntries()
+    {
+        EmployeePayrollData[] arrayOfEmployees = {
+                new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+                new EmployeePayrollData(2, "Bill Gates", 200000.0),
+                new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0)
+        };
+        EmployeePayrollService employeePayrollService;
+        employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmployees));
+        employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
+
+        employeePayrollService.printData(IOService.FILE_IO);
+        long entries = employeePayrollService.countEntries(IOService.FILE_IO);
+        assertEquals(3, entries);
+
+    }
+
+    @Test
+    public void givenFile_WhenRead_ShouldReturnNumberOfEntries() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        long entries = employeePayrollService.readDataFromFile(IOService.FILE_IO);
+        assertEquals(3, entries);
+    }
+
+}
